7deaffdc7aa8c99cb55af080d3c5d2a7f9536459
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-25 09:59:25 -0500

    Update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function populateSquare(square, rows, columns)
+function populateSquareWithSpinnies(square, rows, columns)
 {
 	const colWidth = square.width() / columns;
 	const rowHeight = square.height() / rows;
@@ -20,15 +20,37 @@ function populateSquare(square, rows, columns)
 	}
 }
 
+function populateSquareWithCircles(square, rows, columns)
+{
+	const colWidth = square.width() / columns;
+	const rowHeight = square.height() / rows;
+
+	for (let i = 0; i < rows; ++i)
+	{
+		const typeOfRow = (i % 2 == 0) ? 'row-even' : '';
+		const row = $(`<div class='row ${typeOfRow}' style='height: ${rowHeight}px'></div>`);
+
+		for (let j = 0; j < columns; ++j)
+		{
+			const typeOfColumn = (j % 2 == 0) ? 'col-even' : '';
+			const column = $(`<div class='col ${typeOfColumn}' style='width: ${colWidth}px;'>
+				<div class='circle'></div>
+			</div>`);
+			row.append(column);
+		}
+		square.append(row);
+	}
+}
+
 async function init(rows, columns)
 {
 	// Square 1
 	const sq1 = $('#square1');
-	populateSquare(sq1, rows, columns);
+	populateSquareWithSpinnies(sq1, rows, columns);
 
 	// Square 2
 	const sq2 = $('#square2');
-	populateSquare(sq2, rows, columns);
+	populateSquareWithSpinnies(sq2, rows, columns);
 
 	const sq2Rows = $('#square2 .row');
 	for (let i = 0; i < rows; ++i)
@@ -47,7 +69,7 @@ async function init(rows, columns)
 
 	// Square 3
 	const sq3 = $('#square3');
-	populateSquare(sq3, rows, columns);
+	populateSquareWithSpinnies(sq3, rows, columns);
 
 	const sq3Rows = $('#square3 .row');
 	for (let i = 0; i < rows; ++i)
@@ -67,6 +89,23 @@ async function init(rows, columns)
 			setTimeout(() =>
 			{
 				elem.css({'animation': 'square3 5s linear infinite'});
+			}, i * j * 20);
+		}
+	}
+
+	// Square 4
+	const sq4 = $('#square4');
+	populateSquareWithCircles(sq4, rows, columns);
+
+	const sq4Rows = $('#square4 .row');
+	for (let i = rows - 1; i >= 0; --i)
+	{
+		for (let j = columns - 1; j >= 0; --j)
+		{
+			const elem = $($(sq4Rows[i]).children()[j]);
+			setTimeout(() =>
+			{
+				elem.css({'animation': 'square4 1s linear infinite'});
 			}, i * j * 20);
 		}
 	}
